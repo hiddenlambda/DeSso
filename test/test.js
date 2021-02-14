95a0222c638782b2a0b83b05bab2f9b4834c9e55
Himanshu Garg <garg.himanshu@zomato.com>
2021-02-15 00:33:28 +0530

    refactoring and expanding tests

--- test/test.js
+++ test/test.js
@@ -1,17 +1,7 @@
 const contract = require('@truffle/contract');
 const { expect } = require('chai');
 
-before(() => {
-    this.web3 = require('../commons/web3');
-    authorityContract.setProvider(this.web3.currentProvider);
-    relationshipContract.setProvider(this.web3.currentProvider);
-    identityContract.setProvider(this.web3.currentProvider);
-    identityActionContract.setProvider(this.web3.currentProvider);
-});
-
-after(() => {
-    this.web3.currentProvider.connection.close();
-});
+const web3 = require('../commons/web3');
 
 const BaseAuthority = require('../build/contracts/BaseAuthority.json');
 const authorityContract = contract(BaseAuthority);
@@ -25,80 +15,302 @@ const identityContract = contract(BaseIdentity);
 const IdentityAction = require('../build/contracts/IdentityAction.json');
 const identityActionContract = contract(IdentityAction);
 
-const privateKey = '0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709';
-const privateKey2 = `0x748ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709`;
+authorityContract.setProvider(web3.currentProvider);
+relationshipContract.setProvider(web3.currentProvider);
+identityContract.setProvider(web3.currentProvider);
+identityActionContract.setProvider(web3.currentProvider);
 
-it('created relationship can be retrieved from identity by authority', async () => {
-    const mask = "very-secure-mask-for-pii";
+const Mask = "very-secure-mask-for-pii";
+const Mask2 = "another-secure-mask-for-pii";
 
-    accounts = await this.web3.eth.getAccounts();
-    authorityContractInstance = await authorityContract.deployed();
-    relationshipContractInstance = await relationshipContract.new(
-        authorityContractInstance.contract.options.address,
-        this.web3.utils.fromAscii(mask),
-        this.web3.eth.accounts.privateKeyToAccount(privateKey).address,
-        { from: accounts[1] }
-    );
+const PrivateKey = '0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709';
+const PrivateKey2 = `0x748ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709`;
 
-    identityContractInstance = await identityContract.new(
-        [relationshipContractInstance.contract.options.address],
-        { from: accounts[1] }
-    );
-
-    all = await identityContractInstance.relationshipsTo(
-        authorityContractInstance.contract.options.address
-    );
-    expect(all.length).to.equal(1);
-
-    actualRelationshipInstance = await relationshipContract.at(all[0]);
-    actualMask = await actualRelationshipInstance.mask();
-    expect(this.web3.utils.hexToUtf8(actualMask)).to.equal(mask);
+after(() => {
+    web3.currentProvider.connection.close();
 });
 
-it('able to register new relationship', async () => {
-    accounts = await this.web3.eth.getAccounts();
-    authorityContractInstance = await authorityContract.deployed();
-    relationshipContractInstance = await relationshipContract.new(
-        authorityContractInstance.contract.options.address,
-        this.web3.utils.fromAscii("very-secure-mask-for-pii"),
-        this.web3.eth.accounts.privateKeyToAccount(privateKey).address,
-        { from: accounts[1] }
-    );
-    anotherRelationshipContractInstance = await relationshipContract.new(
-        authorityContractInstance.contract.options.address,
-        this.web3.utils.fromAscii("another-secure-mask-for-pii"),
-        this.web3.eth.accounts.privateKeyToAccount(privateKey2).address,
-        { from: accounts[1] }
-    );
+function cast(instance) {
+    return instance.contract.options.address;
+}
 
-    identityContractInstance = await identityContract.new(
-        [relationshipContractInstance.contract.options.address],
-        { from: accounts[1] }
-    );
+function chooseRandom(choices) {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
 
-    identityActionContractInstance = await identityActionContract.new(
-        identityContractInstance.contract.options.address,
-        0, // register
-        anotherRelationshipContractInstance.contract.options.address,
-        [],
-        { from: accounts[1] }
-    );
+async function getRandomAccount() {
+    accounts = await web3.eth.getAccounts();
+    return chooseRandom(accounts);
+}
 
-    sigObj = this.web3.eth.accounts.sign(this.web3.utils.keccak256(
-        identityActionContractInstance.contract.options.address
-    ), privateKey);
+function accountFor(privateKey) {
+    return web3.eth.accounts.privateKeyToAccount(privateKey).address;
+}
 
-    tx = await identityContractInstance.handle(
-        identityActionContractInstance.contract.options.address,
-        relationshipContractInstance.contract.options.address,
-        sigObj.v,
-        sigObj.r,
-        sigObj.s,
-        { from: accounts[1] }
-    );
+function toBytes(str) {
+    return web3.utils.fromAscii(str);
+}
 
-    all = await identityContractInstance.relationshipsTo(
-        authorityContractInstance.contract.options.address
+async function createRelationship(authority, mask, privateKey) {
+    return await relationshipContract.new(
+        cast(authority),
+        toBytes(mask),
+        accountFor(privateKey),
+        { from: await getRandomAccount() }
     );
-    expect(all.length).to.equal(2);
+}
+
+async function createManyRelationships(details) {
+    authority = await authorityContract.deployed();
+    return await Promise.all(details.map(async ({ mask, key }) => {
+        return await createRelationship(authority, mask, key);
+    }));
+}
+
+async function createIdentity(relationships) {
+    return await identityContract.new(
+        relationships.map(cast),
+        { from: await getRandomAccount() }
+    );
+}
+
+async function createIdentityAction(identity, action, relationship) {
+    return await identityActionContract.new(
+        cast(identity), action, cast(relationship), [],
+        { from: await getRandomAccount() }
+    );
+}
+
+async function createIdentityBulkAction(identity, action, relationships) {
+    return await identityActionContract.new(
+        cast(identity), action, null, relationships.map(cast),
+        { from: await getRandomAccount() }
+    );
+}
+
+function signIdentityAction(identityAction, privateKey) {
+    return web3.eth.accounts.sign(
+        web3.utils.keccak256(cast(identityAction)),
+        privateKey
+    );
+}
+
+async function handleAction(identity, identityAction, proofRelationship, sigObj) {
+    await identity.handle(
+        cast(identityAction),
+        cast(proofRelationship),
+        sigObj.v, sigObj.r, sigObj.s,
+        { from: await getRandomAccount() }
+    );
+}
+
+async function expectMask(relationship, mask) {
+    actualMask = await relationship.mask();
+    expect(web3.utils.hexToUtf8(actualMask)).to.equal(mask);
+}
+
+async function expectAllMasks(relationships, masks) {
+    expect(allRelationships.length).to.equal(masks.length);
+    await Promise.all(relationships.map(async (relation, i) => {
+        actualRelation = await relationshipContract.at(relation);
+        await expectMask(actualRelation, masks[i]);
+    }));
+}
+
+describe('created relationships can be retrieved from identity by authority', async () => {
+    testRoundTrip = (relationshipDetails, masks) => async () => {
+        authority = await authorityContract.deployed();
+
+        relationships = await createManyRelationships(relationshipDetails);
+        identity = await createIdentity(relationships);
+
+        expect(relationships.length).to.equal(relationshipDetails.length);
+
+        allRelationships = await identity.relationshipsTo(cast(authority));
+        await expectAllMasks(allRelationships, masks);
+    }
+    positiveCases = [
+        [{
+            mask: Mask,
+            key: PrivateKey
+        }],
+        [{
+            mask: Mask2,
+            key: PrivateKey2
+        }],
+        [
+            {
+                mask: Mask,
+                key: PrivateKey
+            },
+            {
+                mask: Mask2,
+                key: PrivateKey2
+            },
+        ]
+    ];
+    positiveCases.map(async (c) => {
+        it("all unique relationships are retained", testRoundTrip(c, c.map(o => o.mask)));
+    });
+
+    negativeCases = [
+        {
+            input: [
+                {
+                    mask: Mask,
+                    key: PrivateKey
+                },
+                {
+                    mask: Mask,
+                    key: PrivateKey2
+                },
+            ],
+            output: [
+                Mask
+            ]
+        },
+        {
+            input: [
+                {
+                    mask: Mask,
+                    key: PrivateKey
+                },
+                {
+                    mask: Mask,
+                    key: PrivateKey2
+                },
+                {
+                    mask: Mask2,
+                    key: PrivateKey2
+                }
+            ],
+            output: [
+                Mask,
+                Mask2
+            ]
+        },
+        {
+            input: [
+                {
+                    mask: Mask,
+                    key: PrivateKey
+                },
+                {
+                    mask: Mask2,
+                    key: PrivateKey2
+                },
+                {
+                    mask: Mask,
+                    key: PrivateKey2
+                }
+            ],
+            output: [
+                Mask,
+                Mask2
+            ]
+        }
+    ];
+    negativeCases.map(async (c) => {
+        it("duplicates are dropped", testRoundTrip(c.input, c.output));
+    });
+});
+
+describe('able to register new relationship with correct credentials only', async () => {
+    testRegister = (details, idStartIndex, actionIndex, proofIndex, proofKey, masks) => {
+        return async () => {
+            authority = await authorityContract.deployed();
+
+            relationships = await createManyRelationships(details);
+            expect(relationships.length).to.equal(details.length);
+
+            identity = await createIdentity(relationships.slice(idStartIndex));
+            identityAction = await createIdentityAction(identity, 0, relationships[actionIndex]);
+            sigObj = signIdentityAction(identityAction, proofKey);
+            tx = await handleAction(identity, identityAction, relationships[proofIndex], sigObj);
+
+            allRelationships = await identity.relationshipsTo(cast(authority));
+            await expectAllMasks(allRelationships, masks);
+        }
+    }
+
+    negativeCases = [{
+        title: "Unregistered proof relationship",
+        relationshipDetails: [{
+            mask: Mask,
+            key: PrivateKey
+        }, {
+            mask: Mask2,
+            key: PrivateKey2
+        }],
+        identityPartitionIndex: 1,
+        actionRelationshipIndex: 0,
+        proofRelationshipIndex: 0,
+        proofKey: PrivateKey,
+        masks: [Mask2]
+    },
+    {
+        title: "Unregistered proof relationship with correct private key",
+        relationshipDetails: [{
+            mask: Mask,
+            key: PrivateKey
+        }, {
+            mask: Mask2,
+            key: PrivateKey2
+        }],
+        identityPartitionIndex: 1,
+        actionRelationshipIndex: 0,
+        proofRelationshipIndex: 0,
+        proofKey: PrivateKey2,
+        masks: [Mask2]
+    },
+    {
+        title: "Incorrect private key",
+        relationshipDetails: [{
+            mask: Mask,
+            key: PrivateKey
+        }, {
+            mask: Mask2,
+            key: PrivateKey2
+        }],
+        identityPartitionIndex: 1,
+        actionRelationshipIndex: 0,
+        proofRelationshipIndex: 1,
+        proofKey: PrivateKey,
+        masks: [Mask2]
+    }];
+    negativeCases.map(async (c) => {
+        it("bad credentials - ".concat(c.title), testRegister(
+            c.relationshipDetails,
+            c.identityPartitionIndex,
+            c.actionRelationshipIndex,
+            c.proofRelationshipIndex,
+            c.proofKey,
+            c.masks
+        ));
+    });
+    positiveCases = [{
+        title: "Registered proof relationship and matching private key",
+        relationshipDetails: [{
+            mask: Mask,
+            key: PrivateKey
+        }, {
+            mask: Mask2,
+            key: PrivateKey2
+        }],
+        identityPartitionIndex: 1,
+        actionRelationshipIndex: 0,
+        proofRelationshipIndex: 1,
+        proofKey: PrivateKey2,
+        masks: [Mask2, Mask]
+    }];
+    positiveCases.map(async (c) => {
+        it("good credentials - ".concat(c.title), testRegister(
+            c.relationshipDetails,
+            c.identityPartitionIndex,
+            c.actionRelationshipIndex,
+            c.proofRelationshipIndex,
+            c.proofKey,
+            c.masks
+        ));
+    });
 });
